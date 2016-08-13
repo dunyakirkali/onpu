e500b581c297d91b874c84cf408273e8cec4f849
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-13 23:31:03 +0300

    added auth callback controller

--- app/controllers/jingles_controller.rb
+++ app/controllers/jingles_controller.rb
@@ -1,5 +1,6 @@
 class JinglesController < ApplicationController
   before_action :set_jingle, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /jingles
   # GET /jingles.json
