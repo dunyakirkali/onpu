b1a8ca6e1049f50abcb82ccdb0bccb0cb9228543
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-13 23:38:27 +0300

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :jingles
 
-  # Authenticated
-  authenticated :user do
-    root to: 'users#show', as: :authenticated_root
-  end
-
   root to: 'jingles#index'
 end
