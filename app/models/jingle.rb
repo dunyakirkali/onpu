ce46a2f82bf4eb96408ba49e062eacc06272de4d
Dunya Kirkali <dunyakirkali@gmail.com>
2016-08-13 21:54:27 +0200

    Add validation

--- app/models/jingle.rb
+++ app/models/jingle.rb
@@ -1,4 +1,10 @@
 class Jingle < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  # Relations
+  has_attached_file :audio
+
+  # Validations
+  validates_attachment :mp3, content_type: { content_type: ['audio/mpeg', 'audio/mp3'] }, file_name: { matches: [/mp3\Z/] }
 end
