9f3e0d47fced330868c71899921ed6868f7b58f3
Dunya Kirkali <dunyakirkali@gmail.com>
2016-08-13 21:31:21 +0200

    Fix friendly

--- app/controllers/jingles_controller.rb
+++ app/controllers/jingles_controller.rb
@@ -64,7 +64,7 @@ class JinglesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_jingle
-      @jingle = Jingle.find(params[:id])
+      @jingle = Jingle.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
