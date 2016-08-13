ee53950f68a51d71ca8f976756cf22066e769b89
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-13 23:29:33 +0300

    added devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :jingles
+
+  # Authenticated
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_root
+  end
 
   root to: 'jingles#index'
 end
