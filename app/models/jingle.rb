57cd0485eafabf16470dd7c54e4d632b39ea44c7
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-20 22:23:50 +0300

    added validation

--- app/models/jingle.rb
+++ app/models/jingle.rb
@@ -7,5 +7,6 @@ class Jingle < ApplicationRecord
   belongs_to :user
 
   # Validations
+  validates_attachment_presence :audio
   validates_attachment :audio, content_type: { content_type: ['audio/mpeg', 'audio/mp3'] }, file_name: { matches: [/mp3\Z/] }
 end
