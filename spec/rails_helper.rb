26f17d97b0788baf2dbd8f0b868994ebd8a0260a
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-20 23:22:45 +0300

    include paperclip spec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,11 +5,13 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require "paperclip/matchers"
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Paperclip::Shoulda::Matchers
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
