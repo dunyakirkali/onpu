8a7fb793f0ba1ddd58a16e78f558c5afd37c69f3
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-02 12:37:32 +0200

    Propeer mocks

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -5,6 +5,28 @@ RSpec.configure do |config|
 
   config.before(:each) do
     stub_request(:post, 'https://api.parasut.com/oauth/token?client_id&client_secret&grant_type=password&password&redirect_uri=urn:ietf:wg:oauth:2.0:oob&username')
-      .to_return(status: 200, body: '', headers: {})
+      .to_return(status: 200, body: {}.to_json, headers: {})
+
+    stub_request(:post, 'https://api.parasut.com/oauth/token?client_id&client_secret&grant_type=refresh_token&refresh_token')
+      .to_return(status: 200, body: {}.to_json, headers: {})
+
+    stub_request(:post, 'https://api.parasut.com/v1/products')
+      .to_return(status: 200, body: {
+        product: {
+          id: 1,
+          code: nil,
+          name: 'Gazete - dergi insert',
+          vat_rate: '18.0',
+          currency: 'TRL',
+          list_price: '1500.00',
+          archived: false,
+          category: {
+            id: 5,
+            name: 'Baskı',
+            bg_color: '5cbc68',
+            text_color: 'f3f2f2'
+          }
+        }
+      }.to_json, headers: {})
   end
 end
