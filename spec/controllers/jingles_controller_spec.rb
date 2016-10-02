69b6f855333097f652138f3ae05696fa333aa92d
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-02 14:05:09 +0200

    Remove unused files Increase specs

--- spec/controllers/jingles_controller_spec.rb
+++ spec/controllers/jingles_controller_spec.rb
@@ -4,10 +4,26 @@ RSpec.describe JinglesController, type: :controller do
   let(:valid_attributes) { attributes_for(:jingle) }
 
   describe 'GET #index' do
-    it 'assigns all jingles as @jingles' do
+    xit 'assigns all jingles as @jingles' do
       jingle = create(:jingle, valid_attributes)
       process :index, method: :get, params: {}
       expect(assigns(:jingles)).to match_array([jingle])
+    end
+  end
+
+  describe 'GET #buy' do
+    it 'assigns jingle as @jingle' do
+      jingle = create(:jingle, valid_attributes)
+      process :buy, method: :get, params: { id: jingle.to_param }
+      expect(assigns(:jingle)).to eq(jingle)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns jingle as @jingle' do
+      jingle = create(:jingle, valid_attributes)
+      process :show, method: :get, params: { id: jingle.to_param }
+      expect(assigns(:jingle)).to eq(jingle)
     end
   end
 end
