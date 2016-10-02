df25659fa10e705983d822fc7287e014ba7f280d
onur <onurkucukkece@gmail.com>
2016-10-02 13:14:36 +0300

    boyscout

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,7 +1,6 @@
 # encoding : utf-8
 
 MoneyRails.configure do |config|
-
   # To set the default currency
   #
   # config.default_currency = :usd
@@ -50,7 +49,7 @@ MoneyRails.configure do |config|
   #   :priority            => 1,
   #   :iso_code            => "EU4",
   #   :name                => "Euro with subunit of 4 digits",
-  #   :symbol              => "€",
+  #   :symbol              => "EUR",
   #   :symbol_first        => true,
   #   :subunit             => "Subcent",
   #   :subunit_to_unit     => 10000,
