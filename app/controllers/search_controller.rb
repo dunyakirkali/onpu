7553dc0e0d8886dbe0ec4f4fc5cc54ed9002da3d
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-02 17:27:22 +0200

    Clean up

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
+# SearchController
 class SearchController < ApplicationController
   def jingles
-    @jingles = Jingle.all.includes(:user).where("title LIKE ?", "%#{params[:query]}%").page(params[:page])
+    @jingles = Jingle.all.includes(:user).where('title LIKE ?', "%#{params[:query]}%").page(params[:page])
     respond_to do |format|
       format.js { render layout: false }
       format.html { render 'jingles/index' }
