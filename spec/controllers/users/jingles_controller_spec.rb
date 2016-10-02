5746fc5792c82b269fde9d8cb65c4210f5b1c2ee
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-02 13:30:03 +0200

    Refactor categories

--- spec/controllers/users/jingles_controller_spec.rb
+++ spec/controllers/users/jingles_controller_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe Users::JinglesController, type: :controller do
 
   describe 'GET #index' do
     it 'assigns all categories as @categories' do
-      category = create(:jingle, valid_attributes)
+      jingle = create(:jingle, valid_attributes)
       process :index, method: :get, params: {}
-      expect(assigns(:categories)).to match_array([jingle])
+      expect(assigns(:jingles)).to match_array([jingle])
     end
   end
 
@@ -38,26 +38,26 @@ RSpec.describe Users::JinglesController, type: :controller do
         end.to change(Jingle, :count).by(1)
       end
 
-      it 'assigns a newly created catgory as @category' do
-        process :create, method: :post, params: { category: valid_attributes }
-        expect(assigns(:category)).to be_a(Jingle)
-        expect(assigns(:category)).to be_persisted
+      it 'assigns a newly created catgory as @jingle' do
+        process :create, method: :post, params: { jingle: valid_attributes }
+        expect(assigns(:jingle)).to be_a(Jingle)
+        expect(assigns(:jingle)).to be_persisted
       end
 
-      it 'redirects to the created category' do
-        process :create, method: :post, params: { category: valid_attributes }
+      it 'redirects to the created jingle' do
+        process :create, method: :post, params: { jingle: valid_attributes }
         expect(response).to redirect_to(users_categories_path)
       end
     end
 
     context 'with invalid params' do
-      it 'assigns a newly created but unsaved category as @category' do
-        process :create, method: :post, params: { category: invalid_attributes }
-        expect(assigns(:category)).to be_a_new(Jingle)
+      it 'assigns a newly created but unsaved jingle as @jingle' do
+        process :create, method: :post, params: { jingle: invalid_attributes }
+        expect(assigns(:jingle)).to be_a_new(Jingle)
       end
 
       it "re-renders the 'new' template" do
-        process :create, method: :post, params: { category: invalid_attributes }
+        process :create, method: :post, params: { jingle: invalid_attributes }
         expect(response).to render_template('new')
       end
     end
@@ -65,54 +65,54 @@ RSpec.describe Users::JinglesController, type: :controller do
 
   describe 'PUT #update' do
     context 'with valid params' do
-      let(:new_attributes) { attributes_for(:category) }
+      let(:new_attributes) { attributes_for(:jingle) }
 
-      it 'updates the requested category' do
-        category = create(:jingle, valid_attributes)
-        process :update, method: :put, params: { id: category.to_param, category: new_attributes }
-        category.reload
-        expect(category.name).to eq(new_attributes[:name])
+      it 'updates the requested jingle' do
+        jingle = create(:jingle, valid_attributes)
+        process :update, method: :put, params: { id: jingle.to_param, jingle: new_attributes }
+        jingle.reload
+        expect(jingle.name).to eq(new_attributes[:name])
       end
 
-      it 'assigns the requested category as @category' do
-        category = create(:jingle, valid_attributes)
-        process :update, method: :put, params: { id: category.to_param, category: valid_attributes }
-        expect(assigns(:category)).to eq(category)
+      it 'assigns the requested jingle as @jingle' do
+        jingle = create(:jingle, valid_attributes)
+        process :update, method: :put, params: { id: jingle.to_param, jingle: valid_attributes }
+        expect(assigns(:jingle)).to eq(jingle)
       end
 
-      it 'redirects to the category' do
-        category = create(:jingle, valid_attributes)
-        process :update, method: :put, params: { id: category.to_param, category: valid_attributes }
+      it 'redirects to the jingle' do
+        jingle = create(:jingle, valid_attributes)
+        process :update, method: :put, params: { id: jingle.to_param, jingle: valid_attributes }
         expect(response).to redirect_to(users_categories_path)
       end
     end
 
     context 'with invalid params' do
-      it 'assigns the category as @category' do
-        category = create(:jingle, valid_attributes)
-        process :update, method: :put, params: { id: category.to_param, category: invalid_attributes }
-        expect(assigns(:category)).to eq(category)
+      it 'assigns the jingle as @jingle' do
+        jingle = create(:jingle, valid_attributes)
+        process :update, method: :put, params: { id: jingle.to_param, jingle: invalid_attributes }
+        expect(assigns(:jingle)).to eq(jingle)
       end
 
       it "re-renders the 'edit' template" do
-        category = create(:jingle, valid_attributes)
-        process :update, method: :put, params: { id: category.to_param, category: invalid_attributes }
+        jingle = create(:jingle, valid_attributes)
+        process :update, method: :put, params: { id: jingle.to_param, jingle: invalid_attributes }
         expect(response).to render_template('edit')
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    it 'destroys the requested category' do
-      category = create(:jingle, valid_attributes)
+    it 'destroys the requested jingle' do
+      jingle = create(:jingle, valid_attributes)
       expect do
-        process :destroy, method: :delete, params: { id: category.to_param }
+        process :destroy, method: :delete, params: { id: jingle.to_param }
       end.to change(Jingle, :count).by(-1)
     end
 
     it 'redirects to the tags list' do
-      category = create(:jingle, valid_attributes)
-      process :destroy, method: :delete, params: { id: category.to_param }
+      jingle = create(:jingle, valid_attributes)
+      process :destroy, method: :delete, params: { id: jingle.to_param }
       expect(response).to redirect_to(users_categories_path)
     end
   end
