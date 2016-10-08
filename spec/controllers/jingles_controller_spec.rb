11273239f26cdfae3bfb6ad471a8f8aa0e30ed3a
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-08 19:18:29 +0200

    REmove unused test

--- spec/controllers/jingles_controller_spec.rb
+++ spec/controllers/jingles_controller_spec.rb
@@ -3,14 +3,6 @@ require 'rails_helper'
 RSpec.describe JinglesController, type: :controller do
   let(:valid_attributes) { attributes_for(:jingle) }
 
-  describe 'GET #index' do
-    xit 'assigns all jingles as @jingles' do
-      jingle = create(:jingle, valid_attributes)
-      process :index, method: :get, params: {}
-      expect(assigns(:jingles)).to match_array([jingle])
-    end
-  end
-
   describe 'GET #buy' do
     it 'assigns jingle as @jingle' do
       jingle = create(:jingle, valid_attributes)
