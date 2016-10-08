ac978c9dcbc1c976c20d7180e2e081753783599a
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-08 13:48:25 +0200

    Complete specs

--- spec/features/guest/sidebar_spec.rb
+++ spec/features/guest/sidebar_spec.rb
@@ -6,27 +6,25 @@ RSpec.describe 'Guest', type: :feature do
   end
 
   describe 'can toggle sidebar' do
+    before :each do
+      find('#sidebar').click
+    end
+
     context 'when already visible' do
-      it 'via the logo' do
-        within '#sidebar' do
-          find('.logo').click
-        end
+      it 'via the bar' do
+        find('#sidebar').click
         expect(page).not_to have_css('#sidebar.visible')
       end
 
       it 'via body' do
-        within '#sidebar' do
-          find('body').click
-        end
+        find('body').click
         expect(page).not_to have_css('#sidebar.visible')
       end
     end
 
     context 'when not visible' do
       it 'via the logo' do
-        within '#sidebar' do
-          find('.logo').click
-        end
+        find('#sidebar').click
         expect(page).to have_css('#sidebar.visible')
       end
     end
