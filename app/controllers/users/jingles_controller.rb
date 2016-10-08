459faf692088f8394e34daaaf7266bb0dd902594
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-08 18:01:52 +0200

    Refactoring jingles controller

--- app/controllers/users/jingles_controller.rb
+++ app/controllers/users/jingles_controller.rb
@@ -2,68 +2,50 @@
 module Users
   # JinglesController
   class JinglesController < ApplicationController
+    layout false
+
     before_action :set_jingle, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!
     skip_before_action :verify_authenticity_token, only: [:create, :update]
     protect_from_forgery only: [:create, :update] if Rails.env.test?
 
     def new
-      @jingle = current_user.jingles.build
-      render layout: false
+      @jingle = scope.build
     end
 
     def edit
-      render layout: false
     end
 
     def create
-      @jingle = current_user.jingles.build(jingle_params)
-      respond_to do |format|
-        if @jingle.save
-          format.js do
-            set_jingles
-            render layout: false
-          end
-          format.html { redirect_to search_jingles_path, notice: 'Jingle was successfully created.' }
-        else
-          format.js { render layout: false }
-        end
-      end
+      @jingle = scope.build(jingle_params)
+      set_jingles if @jingle.save
     end
 
     def update
-      respond_to do |format|
-        if @jingle.update(jingle_params)
-          format.js do
-            set_jingles
-            render layout: false
-          end
-          format.html { redirect_to search_jingles_path, notice: 'Jingle was successfully updated.' }
-        else
-          format.js { render layout: false }
-        end
-      end
+      set_jingles if @jingle.update(jingle_params)
     end
 
     def destroy
-      @jingle.destroy
-      set_jingles
-      render layout: false
+      set_jingles if @jingle.destroy
     end
 
     private
 
     def set_jingles
-      @jingles = Jingle.all.includes(:user).page(params[:page])
+      @jingles = Jingle.includes(:user).page(params[:page])
     end
 
     def set_jingle
-      @jingle = current_user.jingles.friendly.find(params[:id])
+      @jingle = scope.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def jingle_params
       params.require(:jingle).permit(:title, :audio, :price, :cover)
+    end
+
+    def scope
+      current_user.jingles
     end
   end
 end
