e1526844b90e0d77b8b8f240bd001bc080da3a23
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-09 19:46:02 +0200

    Add basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  before_action :authenticate, if: :should_authenticate?
+
+  protected
+
+  def should_authenticate?
+    ENV['AUTH_USERNAME'].present? && ENV['AUTH_PASSWORD'].present?
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['AUTH_USERNAME'] && password == ENV['AUTH_PASSWORD']
+    end
+  end
 end
