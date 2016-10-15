839eaf12bc9741d4f61d8c2a8dd5d7fe31dbc586
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-15 20:44:19 +0200

    Increase seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ puts 'deleting Users'
 User.delete_all
 puts 'deleting Jingles'
 Jingle.delete_all
-
-
+puts 'creating 25 Jingles'
+FactoryGirl.create_list(:jingle, 25)
+puts 'creating User with 3 jingles'
 FactoryGirl.create_list(:user, 3, :with_jingels)
