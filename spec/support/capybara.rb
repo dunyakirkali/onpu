88dd2775a01608e7aa76fc091543fcb884172681
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-15 17:19:30 +0200

    Increase capbara screen size

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,9 @@
 require 'capybara/poltergeist'
+Capybara.register_driver :poltergeist do |app|
+  options = {
+    screen_size: [1600, 1200]
+  }
+  Capybara::Poltergeist::Driver.new(app, options)
+end
 Capybara.javascript_driver = :poltergeist
 Capybara.raise_server_errors = false
